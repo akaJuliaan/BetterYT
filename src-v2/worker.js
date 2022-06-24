b67336d524f6a534281e021a913556428cc07590
Julian <info@versahl.at>
2022-06-24 20:25:36 +0200

    start debugging

--- src-v2/worker.js
+++ src-v2/worker.js
@@ -1,13 +1,63 @@
+let e = [];
+
 function init() {
   loadEmotes().then((emotes) => {
+    e = emotes;
     console.log("Successfully loaded Emotes!");
     findChatFrame().then((chat) => {
       console.log("Found Chat-Frame!");
+      setInterval(() => {
+        for (const message of chat.getElementById("chat-messages").querySelectorAll("yt-live-chat-text-message-renderer")) {
+          replaceEmotes(message, emotes);
+        }
+      }, 500);
     });
   });
 }
 
-function replaceEmotes(message) {}
+function replaceEmotes(message, emotes) {
+  for (const emote of emotes) {
+    let content = message.querySelector("#message");
+    for (const node of content.childNodes) {
+      setTimeout(() => {
+        if (node.nodeType != Node.TEXT_NODE) return;
+        const nodeContent = node.nodeValue;
+        if (!nodeContent) return;
+        if (nodeContent.includes(emote.name)) {
+          //Wie geil Pog hahaha
+          const begin = nodeContent.indexOf(emote.name);
+          const end = begin + emote.name.length;
+
+          const textAfterEmote = nodeContent.substring(end, nodeContent.length);
+          const textBeforeEmote = nodeContent.substring(0, begin);
+
+          node.nodeValue = nodeContent.replace(emote.name, "");
+
+          //let newNode = content.cloneNode(true);
+          let newNode = document.createElement("span");
+          //newNode.setAttribute("id", content.id);
+
+          let img = document.createElement("IMG");
+          img.setAttribute("src", emote.link);
+          img.setAttribute("alt", emote.name);
+          img.setAttribute("style", "vertical-align:middle");
+          img.setAttribute("class", "yt-formatted-string style-scope yt-live-chat-text-message-renderer");
+          img.setAttribute("shared-tooltip-text", ":BetterYT:");
+          newNode.appendChild(document.createTextNode(textBeforeEmote));
+          newNode.appendChild(img);
+          newNode.appendChild(document.createTextNode(textAfterEmote));
+
+          content.replaceChild(document.createTextNode(textBeforeEmote), node);
+          const emote_img = content.insertBefore(img, node.nextSibling);
+          content.insertBefore(document.createTextNode(textAfterEmote), emote_img.nextSibling);
+
+          //content.replaceWith(newNode);
+          //replaceEmotes(message, e); //if more than 1 emote
+        }
+      }, 500);
+    }
+  }
+}
 
 function loadEmotes() {
   return new Promise((resolve, reject) => {
