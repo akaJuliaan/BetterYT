abdbd44965d58da5a88456404ccf7a425435101d
Julian <info@versahl.at>
2022-06-26 15:53:51 +0200

    avoid innerHTML

--- src-v2/donation.js
+++ src-v2/donation.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("msg").innerHTML = chrome.i18n.getMessage("appDonation");
+  document.getElementById("msg").textContent = chrome.i18n.getMessage("appDonation");
 
   document.body.addEventListener("click", (e) => {
     if (e.target.tagName === "A") {
