d5563666d461bd02c30ff30543b37f490b00dc94
Julian <info@versahl.at>
2022-06-26 15:33:14 +0200

    style donation popup

--- src-v2/donation.js
+++ src-v2/donation.js
@@ -1,3 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('msg').innerHTML = chrome.i18n.getMessage('appDonation');
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("msg").innerHTML = chrome.i18n.getMessage("appDonation");
+
+  document.body.addEventListener("click", (e) => {
+    if (e.target.tagName === "A") {
+      setTimeout(() => {
+        window.close();
+      }, 100);
+    }
+  });
 });
